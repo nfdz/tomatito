c5305cb8cfc5b0e6ae7ddab719ec9372285b7ead
nfdz <noe.fernandez@protonmail.com>
2017-06-19 11:01:14 +0200

    start to implement some tests

--- Tomatito/app/src/androidTest/java/io/github/nfdz/tomatito/ExampleInstrumentedTest.java
+++ Tomatito/app/src/androidTest/java/io/github/nfdz/tomatito/ExampleInstrumentedTest.java
@@ -1,12 +1,37 @@
 package io.github.nfdz.tomatito;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.SystemClock;
+import android.preference.PreferenceManager;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.PerformException;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.util.HumanReadables;
+import android.support.test.espresso.util.TreeIterables;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
 
+import org.hamcrest.Matcher;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.TimeoutException;
+
+import io.github.nfdz.tomatito.data.PreferencesUtils;
+import io.github.nfdz.tomatito.ui.MainActivity;
+
+import static android.support.test.InstrumentationRegistry.getContext;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -15,6 +40,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
@@ -23,4 +49,107 @@ public class ExampleInstrumentedTest {
 
         assertEquals("io.github.nfdz.tomatito", appContext.getPackageName());
     }
+
+
+    Intent intent;
+    SharedPreferences.Editor preferencesEditor;
+
+    /**
+     * {@link ActivityTestRule} is a JUnit {@link Rule @Rule} to launch your activity under test.
+     *
+     * <p>
+     * Rules are interceptors which are executed for each test method and are important building
+     * blocks of Junit tests.
+     */
+    @Rule
+    public ActivityTestRule<MainActivity> mMainActivityTestRule = new ActivityTestRule<>(MainActivity.class, true, false);
+
+    @Before
+    public void setUp() {
+        intent = new Intent();
+        // create a SharedPreferences editor
+        preferencesEditor = PreferenceManager.getDefaultSharedPreferences(getContext()).edit();
+    }
+
+    @Test
+    public void clickStartPomodoro() throws Exception {
+
+        mMainActivityTestRule.launchActivity(intent);
+
+        long now = System.currentTimeMillis();
+        // Click on the start pomodoro button
+        onView(isRoot()).perform(waitFor(2000));
+        onView(withId(R.id.fab_start_pomodoro)).perform(click());
+
+        // Check if the pomodoro has started
+        SystemClock.sleep(2500);
+        long currentPomodoro = PreferencesUtils.getCurrentPomodoro(getContext());
+        assertTrue("Pomodoro has started, currentPomodoro: "+currentPomodoro,
+                currentPomodoro != PreferencesUtils.CURRENT_POMODORO_DEFAULT);
+    }
+
+    /**
+     * Perform action of waiting for a specific time.
+     */
+    public static ViewAction waitFor(final long millis) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isRoot();
+            }
+
+            @Override
+            public String getDescription() {
+                return "Wait for " + millis + " milliseconds.";
+            }
+
+            @Override
+            public void perform(UiController uiController, final View view) {
+                uiController.loopMainThreadForAtLeast(millis);
+            }
+        };
+    }
+
+    /** Perform action of waiting for a specific view id. */
+    public static ViewAction waitId(final int viewId, final long millis) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return isRoot();
+            }
+
+            @Override
+            public String getDescription() {
+                return "wait for a specific view with id <" + viewId + "> during " + millis + " millis.";
+            }
+
+            @Override
+            public void perform(final UiController uiController, final View view) {
+                uiController.loopMainThreadUntilIdle();
+                final long startTime = System.currentTimeMillis();
+                final long endTime = startTime + millis;
+                final Matcher<View> viewMatcher = withId(viewId);
+
+                do {
+                    for (View child : TreeIterables.breadthFirstViewTraversal(view)) {
+                        // found view with required ID
+                        if (viewMatcher.matches(child)) {
+                            return;
+                        }
+                    }
+
+                    uiController.loopMainThreadForAtLeast(50);
+                }
+                while (System.currentTimeMillis() < endTime);
+
+                // timeout happens
+                throw new PerformException.Builder()
+                        .withActionDescription(this.getDescription())
+                        .withViewDescription(HumanReadables.describe(view))
+                        .withCause(new TimeoutException())
+                        .build();
+            }
+        };
+    }
+
 }
---
+++ Tomatito/app/src/main/java/io/github/nfdz/tomatito/utils/PomodoroUtils.java
@@ -0,0 +1,4 @@
+package io.github.nfdz.tomatito.utils;
+
+public class PomodoroUtils {
+}
