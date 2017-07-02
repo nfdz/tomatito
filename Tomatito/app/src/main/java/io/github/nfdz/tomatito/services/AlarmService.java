79dd7e53e87e7e590db06e8069881fad2d1421b4
nfdz <noe.fernandez@protonmail.com>
2017-07-02 02:54:07 +0200

    add vibrations

--- Tomatito/app/src/main/java/io/github/nfdz/tomatito/services/AlarmService.java
+++ Tomatito/app/src/main/java/io/github/nfdz/tomatito/services/AlarmService.java
@@ -1,7 +1,9 @@
 package io.github.nfdz.tomatito.services;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Vibrator;
 
 import io.github.nfdz.tomatito.utils.AlarmUtils;
 import io.github.nfdz.tomatito.utils.NotificationUtils;
@@ -14,6 +16,8 @@ public class AlarmService extends IntentService {
     public static final String LONG_BREAK_ALARM = "long-break";
     public static final String END_ALARM = "end";
 
+    private static final long[] VIBRATION_PATTERN = { 0, 200, 500, 200, 500, 200, 500, 200, 500 };
+    
     public AlarmService() {
         super(AlarmService.class.getSimpleName());
     }
@@ -23,6 +27,13 @@ public class AlarmService extends IntentService {
         String action = intent.getAction();
         Timber.d("Alarm service triggered, action: " + action);
 
+        // vibrate
+        // TODO check preference
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (v != null && v.hasVibrator()) v.vibrate(VIBRATION_PATTERN , -1);
+
+        // notify
+        // TODO check preference
         switch(action) {
             case WORK_ALARM:
                 NotificationUtils.notifyWork(this);
